79b57c325279e7d3eee2867dc001ec4cfe7b538d
Victor Ratts <victor.ratts13@gmail.com>
2018-12-19 17:22:18 -0200

    Add files via upload

---
+++ reload.js
@@ -0,0 +1,15 @@
+// Js Reaload 1.0
+//escrito por Victor Ratts
+
+/*
+como usar:
+
+1 - configure 'clock.php' para a pagina que deseja carregar (ou recarregar).
+2 - defina '#reload' como uma id dentro de uma div da sua pagina index (a pagina onde deseja mostrar as funções recarregadas).
+3 - defina o time do reaload (por padrão é 1 segundo ou 1000 milisegundos).
+
+
+*/ 
+window.setInterval(function(){		
+		$('#reload').load('clock.php');
+	}, 1000);
