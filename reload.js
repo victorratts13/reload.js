c7b6668e8f36d181750a89fd3b12568bfcf94085
Victor Ratts <victor.ratts13@gmail.com>
2018-12-19 17:23:47 -0200

    adicione Jquery no seu projeto

--- reload.js
+++ reload.js
@@ -7,7 +7,7 @@ como usar:
 1 - configure 'clock.php' para a pagina que deseja carregar (ou recarregar).
 2 - defina '#reload' como uma id dentro de uma div da sua pagina index (a pagina onde deseja mostrar as funções recarregadas).
 3 - defina o time do reaload (por padrão é 1 segundo ou 1000 milisegundos).
-
+4 - não esqueça de adcionar o Jquery em seu projeto
 
 */ 
 window.setInterval(function(){		
